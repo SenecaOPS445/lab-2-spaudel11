763559d5335d2c53b90a48914da5d066e3466181
Samip Paudel <sss.detonator@gmail.com>
2025-01-25 02:14:34 -0500

    added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Samip Paudel
+#Author ID: 168727212
+#Date Created: 2024/05/24
+
+import sys
+timer = int(sys.argv[1])
+
+while timer !=0:
+    print(timer)
+    timer -= 1
+
+print('blast off!')
