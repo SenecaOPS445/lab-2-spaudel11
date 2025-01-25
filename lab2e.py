1d363dafdf161549080ab15b274d70a42a36d6da
Samip Paudel <sss.detonator@gmail.com>
2025-01-25 02:08:25 -0500

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Samip Paudel
+#Author ID: 168727212
+#Date Created: 2024/05/24
+
+timer = 10
+
+while timer !=0:
+   print(timer)
+   timer -=1
+
+print('blast off!')
