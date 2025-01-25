db4a58c5ffbbc46b54b4f7a3624c84480acfe487
Samip Paudel <sss.detonator@gmail.com>
2025-01-25 02:34:05 -0500

    added to lab2g.py

---
+++ lab2g.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#Author: Samip Paudel
+#Author ID: 168727212
+#Date Created: 2024/05/24
+
+import sys
+
+if len(sys.argv) == 1:
+    timer = 3
+elif len(sys.argv) == 2:
+    timer = int(sys.argv[1])
+else:
+    print("timer")
+    sys.exit(1)
+
+while timer > 0:
+    print(f"{timer}")
+    timer -= 1
+
+print('blast off!')
